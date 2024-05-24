03f11d5234943341a88f659bb4be12c1dbdd58f5
Evelyn Santoso <velyn.es24@gmail.com>
2024-05-24 22:52:30 +0700

    Initial Commit

---
+++ sunbet Watch App/sunbetApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunbetApp.swift
+//  sunbet Watch App
+//
+//  Created by Evelyn Santoso on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct sunbet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
